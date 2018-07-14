f04d15dcdd2bbb13bd0700cc35b7a38a5810e5ee
chi_Script <chinedu.onyeoka@gmail.comgit config --global user.name chibaba>
2018-07-14 12:21:28 +0100

    modified

--- test/api/accounTest.js
+++ test/api/accounTest.js
@@ -8,6 +8,25 @@ const server = require('../../');
 const should = chai.should();
 chai.use(chaiHttp);
 
+describe('POST /signup', () => {
+  it('it should able to register new account', done => {
+    const params = {
+      email: 'chibaba@gmail.com',
+      password: 'we are here',
+    };
+    chai
+      .request('http://localhost:5000')
+      .post('/signup')
+      .send(params)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        //res.body.should.have.property('token');
+        done();
+      });
+  });
+});
+
 describe('Login/SignUp Routes', () => {
   describe('POST /login', () => {
     it('it should POST a login ', done => {
@@ -18,24 +37,6 @@ describe('Login/SignUp Routes', () => {
       chai
         .request('http://localhost:5000')
         .post('/login')
-        .send(params)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a('object');
-          //res.body.should.have.property('token');
-          done();
-        });
-    });
-  });
-  describe('POST /signup', () => {
-    it('it should able to register new account', done => {
-      const params = {
-        email: 'chibaba@gmail.com',
-        password: 'we are here',
-      };
-      chai
-        .request('http://localhost:5000')
-        .post('/signup')
         .send(params)
         .end((err, res) => {
           res.should.have.status(200);
