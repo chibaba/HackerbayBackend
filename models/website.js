d6db757b0944f0e4a2bc10c2de3fde701671bb6b
chi_Script <chinedu.onyeoka@gmail.comgit config --global user.name chibaba>
2018-10-11 01:35:21 +0100

    database migrations

---
+++ models/website.js
@@ -0,0 +1,11 @@
+export default (sequelize, DataTypes) => {
+  var Website = sequelize.define('Website', {
+    name: DataTypes.TEXT,
+    url: DataTypes.TEXT,
+    status: DataTypes.ENUM('Online', 'Offline')
+  }, {});
+  Website.associate = function(models) {
+    Website.belongsTo(models.user);
+  };
+  return Website;
+};
